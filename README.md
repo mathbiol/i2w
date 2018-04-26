0329f2906b57d61a8feabcb32bb75a579b062520
Jonas Almeida <jalmeida@mathbiol.org>
2018-04-26 15:11:52 -0400

    browser import

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # i2w
-i2workshop dem package
+i2workshop demo package
+
+for browser native import check [https://mathbiol.github.io/i2w]
---
+++ i2wb.js
@@ -0,0 +1,9 @@
+/*
+export const hello=()=>{
+    return 'hello from i2wb at '+Date()
+}
+*/
+export const hello = () => {
+  console.log('saying hello …');
+  return 'hello from i2wb at '+Date()
+};
