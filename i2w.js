f7eef3afe970fbdb02e44651f2f47c74ae74c59e
Jonas Almeida <jalmeida@mathbiol.org>
2018-04-26 10:18:26 -0400

    moving to import

--- i2w.js
+++ i2w.js
@@ -3,18 +3,32 @@ console.log('i2w.js loaded :-)')
 i2w=function(){
     return 'hello at '+Date()
 }
-i2w.ole=function(){
-    return 'ole at '+Date()
+i2w.ole=function(){   
+    return 'ole att '+Date()
 }
 
 console.log(i2w())
+console.log(i2w.ole())
+
+if(!typeof(define)){
+    define({
+        hello:i2w.hello,
+        hello:i2w.ole
+    }) 
+}
+
+if(!typeof(exports)){
+    exports.hello=i2w.hello
+    exports.hello=i2w.ole
+}
+    
 
 /*
-if(exports){
-    exports.hello=i2w
-}
-
-export {
-    i2w
-}
+http = require('http')
+http.createServer(function(req,res){
+    let lala='lala'
+    res.write(i2w.ole())
+    res.end('Hello World att '+Date());
+    console.log('called at '+Date())
+}).listen(8001)
 */
