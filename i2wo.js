8ce5f60ed0b6893469555914573e605f732fcb01
Jonas Almeida <jalmeida@mathbiol.org>
2018-04-26 16:56:37 -0400

    hello require amd

--- i2wo.js
+++ i2wo.js
@@ -1,13 +1,12 @@
 console.log('loaded i2wo.js ... maybe from observable')
 
 //if(typeof(define)!=='undefined')){
-    define(function(){
-        return{
+    define({
             hello:function(){
                 var h = 'hello at '+Date()
                 console.log(h)
                 return h
             }
-        }
+        
     })
 //}
