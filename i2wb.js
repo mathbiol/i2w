82af5c064844acae3bd0981ad3a573fa11badecb
Jonas Almeida <jalmeida@mathbiol.org>
2018-04-26 15:22:47 -0400

    v .10 > .11

--- i2wb.js
+++ i2wb.js
@@ -5,5 +5,5 @@ export const hello=()=>{
 */
 export const hello = () => {
   console.log('saying hello …');
-  return 'hello from i2wb at '+Date()
+  return 'hello from <a href="https://github.com/mathbiol/i2w/blob/master/index.html" target="_blank">importing</a> from module <a href="https://github.com/mathbiol/i2w/blob/master/i2wb.js" target="_blank">i2wb</a> <br>at '+Date()+ ' <br>for node require equivalent see <a href="https://www.npmjs.com/package/i2w" target="_blank">npm</a>.'
 };
