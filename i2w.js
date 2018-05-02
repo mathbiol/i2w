d373c8a5c1f7cff25e601d2f04e0a1730a28d127
Jonas Almeida <jalmeida@mathbiol.org>
2018-05-02 09:34:59 -0400

    hello in more languages

--- i2w.js
+++ i2w.js
@@ -2,5 +2,13 @@ console.log('i2w.js loaded :-)')
 
 exports.hello = function(){
     return 'Hello from i2w at '+Date()
-}   
+}
+
+exports.Hola = function(){
+    return 'Hola de i2w desde '+Date()
+} 
+
+exports.Ola = function(){
+    return 'Olá do i2w às '+Date()
+} 
 
